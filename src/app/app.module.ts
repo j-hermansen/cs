4e18006a4f340c9b5487fd7f43c25ff4e748c506
Jonathan Hermansen <jhermansen@live.no>
2021-06-27 01:15:51 +0200

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule} from '@angular/material/card';
 import { Angular2Component } from './angular2/angular2.component';
 import { StartComponent } from './start/start.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/start', pathMatch: 'full'},
+  { path: 'start', component: StartComponent},
+  { path: 'angular2', component: Angular2Component}
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +23,8 @@ import { StartComponent } from './start/start.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatCardModule
+    MatCardModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
